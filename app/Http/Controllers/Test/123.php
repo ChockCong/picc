b094869b7aa98713f2bb31658bc0f727be4ca8fe
Administrator <Administrator@PICC-PC>
2017-06-16 14:51:08 +0800

    1

---
+++ app/Http/Controllers/Test/123.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/6/16 0016
+ * Time: 11:11
+ */
+
+?>
+<a href="http://dcsapi.com?k=147168040&url=http://123.207.33.105/奖励查询报表样例.xlsx">链接</a>
